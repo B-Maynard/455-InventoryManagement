373fd596fe910b7f0d4fe77fd21c04bd6ea9c1b8
Bailey Maynard <bailey_maynard@live.com>
2018-04-23 15:04:25 -0400

    rough first layout

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+  <p>&copy;OnlineRetail 2017. All Rights Reserved</p>
+</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,37 @@
+<?php require "header.php" ?>
+
+<body>
+  <div id="top">
+    <img id="logo" src="https://www.graphicsprings.com/filestorage/stencils/fef1c295c28fa66259d253156c63a459.svg" alt="logo">
+    <br>
+    <h1>OnlineRetail</h1>
+  </div>
+  <div id="featured-items">
+    <div class="indiv-item">
+      <img class="product-img" src="https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05509300.png" alt="">
+      <h4>Item Name</h4>
+      <h5>$0.00</h5>
+    </div>
+    <div class="indiv-item">
+      <img class="product-img" src="https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05509300.png" alt="">
+      <h4>Item Name</h4>
+      <h5>$0.00</h5>
+    </div>
+    <div class="indiv-item">
+      <img class="product-img" src="https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05509300.png" alt="">
+      <h4>Item Name</h4>
+      <h5>$0.00</h5>
+    </div>
+    <div class="indiv-item">
+      <img class="product-img" src="https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05509300.png" alt="">
+      <h4>Item Name</h4>
+      <h5>$0.00</h5>
+    </div>
+    <div class="indiv-item">
+      <img class="product-img" src="https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05509300.png" alt="">
+      <h4>Item Name</h4>
+      <h5>$0.00</h5>
+    </div>
+  </div>
+  <div id="bottom">
+  </div>
