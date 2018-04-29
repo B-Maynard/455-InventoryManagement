8c67903650b301637966b03740eae8c1112e3e78
Bailey Maynard <bailey_maynard@live.com>
2018-04-29 15:39:45 -0400

    comment fix

--- utilities.php
+++ utilities.php
@@ -1,6 +1,7 @@
 <?php
 
 function getConnection() {
+  //using local database for now, plan to change to hosted database
   $connection = new mysqli(
     "localhost:3306", //host
     "root", //username
